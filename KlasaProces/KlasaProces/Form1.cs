c7541746170b2e3f3eb202204ebae554a6307ea7
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-04-26 09:32:00 +0200

    Add files via upload

---
+++ KlasaProces/KlasaProces/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace KlasaProces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process.Start(startInfo);
+        }
+
+        private void buttonExcel_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("Excel.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process.Start(startInfo);
+        }
+
+        private void buttonWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("WINWORD.exe");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("WINWORD.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process.Start(startInfo);
+        }
+
+        private void buttonStartInfo_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+            Process.Start(startInfo);
+        }
+    }
+}
